fc2cb88753e9035ef11132b2703a65c8fb7d1602
Milan Bharanya <XMBomb@users.noreply.github.com>
2017-09-27 12:49:43 +0200

    remove tee

--- link/i3/wallpaper.sh
+++ link/i3/wallpaper.sh
@@ -1,10 +1,10 @@
+#!/bin/bash
 mv *.png *.jpg *.jpeg *.gif old
-curl -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36" -s "https://www.reddit.com/r/MinimalWallpaper+Offensive_Wallpapers+WQHD_Wallpaper+nocontext_wallpapers+topwalls+ultrahdwallpapers+wallpaper+wallpaperdump+wallpapers/top/?sort=top&t=day" \
-  | tr '"' '\n' | grep imgur | tee "$$.tmp" \
+curl -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36" -s "https://www.reddit.com/r/ImaginaryStarscapes+MinimalWallpaper+Offensive_Wallpapers+WQHD_Wallpaper+nocontext_wallpapers+topwalls+ultrahdwallpapers+wallpaper+wallpaperdump+wallpapers/top/?sort=top&t=day" \
+  | tr '"' '\n' | grep imgur \
   | egrep '^https?://i\.imgur\.com/' \
   | xargs wget -q -nc
 egrep '^http://imgur.com/a/' "$$.tmp" \
   | xargs -l1 ./imgur_album_download.sh
-rm "$$.tmp"
 rename 's/(.*)\?[0-9]/$1/g' *
 rename 's/\)//g' *
